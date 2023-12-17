061c24baa2d766ae5b38a397b4f0b4f9d1547a3c
dcct0r <ckppaaa@gmail.com>
2023-12-17 23:22:35 +0300

    task completed

--- src/main/java/com/example/autoservice/domain/repository/CarBodyRepository.java
+++ src/main/java/com/example/autoservice/domain/repository/CarBodyRepository.java
@@ -1,10 +1,12 @@
 package com.example.autoservice.domain.repository;
 
 import com.example.autoservice.domain.model.CarBody;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CarBodyRepository {
     List<CarBody> getBodyCars();
     CarBody getCarBodyById(UUID id);
--- src/main/java/com/example/autoservice/domain/repository/CarWheelRepository.java
+++ src/main/java/com/example/autoservice/domain/repository/CarWheelRepository.java
@@ -2,10 +2,12 @@ package com.example.autoservice.domain.repository;
 
 
 import com.example.autoservice.domain.model.CarWheel;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CarWheelRepository {
     List<CarWheel> getCarWheels();
     CarWheel getCarWheelById(UUID id);
